8231839f594da3f254217dc26dfce2041c684a21
nano <nanoleinz@gmail.com>
2024-06-09 20:48:57 +0700

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Harga Crypto</h1>
+    <button class="btn btn-primary" @click="fetchData">Get Data</button>
+    <table class="table table-striped mt-3">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Price USD</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="coin in coins" :key="coin.id">
+          <td>{{ coin.name }}</td>
+          <td>{{ coin.symbol }}</td>
+          <td>{{ coin.price_usd }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      coins: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get("https://api.coinlore.net/api/tickers/")
+        .then((response) => {
+          this.coins = response.data.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+table {
+  margin: 20px auto;
+  width: 80%;
 }
 </style>
